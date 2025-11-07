39c724a8e62edf49b91170ed8faaae73d0fe320e
pp17 <173219513@qq.com>
2025-11-07 18:23:26 +0800

    Refactor transferExecution function signature
    Removed unused parameter from transferExecution function.

--- src/Engine/Engine.h
+++ src/Engine/Engine.h
@@ -335,7 +335,7 @@ public:
    *               after call.
    */
   void reduceCacheTo(uint32_t nb);
-  bool transferExecution(void* execBrokerPtr, rword addr, GPRState* gprState, FPRState* fprState);
+  bool transferExecution(rword addr, GPRState* gprState, FPRState* fprState);
 };
 
 } // namespace QBDI
